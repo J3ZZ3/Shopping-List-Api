aa4e09c7499b1b274da891ef61c89a2b435e188d
Jesse <jesse.mashoana@gmail.com>
2024-10-29 10:46:53 +0200

    fixed

--- shopping-list-api/getNextId.js
+++
@@ -1,7 +0,0 @@
-const getNextId = (shoppingList) => {
-    if (shoppingList.length === 0) return 1;
-    const ids = shoppingList.map(item => item.id);
-    return Math.max(...ids) + 1;
-  };
-
-  module.exports = getNextId;
